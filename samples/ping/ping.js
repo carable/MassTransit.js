6bc2e0619f44f49627f048a5be52079094ce700b
Brian Rumburg <brian@rumburg.org>
2016-05-13 16:51:41 -0500

    Added support for multiple message types

--- samples/ping/ping.js
+++ samples/ping/ping.js
@@ -1,29 +1,41 @@
 var bus = require('../../lib').create();
 
 bus.ready(function() {
-	bus.subscribe({queueName:'ping-test',messageType:'PingMassTransit:Pong'}, function(msg) {
-		console.log('pong received on ping-test');
-		console.log(msg);
-		//process.exit();
+
+	bus.subscribe({
+		queueName: 'ping-test',
+		messageType: 'PingMassTransit:IPong'
+	}, function(message, context, queueName) {
+		console.log('message destined for [' + context.destinationAddress + '] received on queue [' + queueName + ']');
+		console.log(message);
 	});
 
-	bus.subscribe('PingMassTransit:Pong', function(msg) {
-		console.log('pong received on default queue');
-		console.log(msg);
-	  throw "Consumer Error";
+	bus.subscribe({
+		queueName: 'ping-test2',
+		messageType: 'PingMassTransit:Pong2'
+	}, function(message, context, queueName) {
+		console.log('message destined for [' + context.destinationAddress + '] received on queue [' + queueName + ']');
+		console.log(message);
 	});
-
 
 	bus.publish('PingMassTransit:Ping', {
 		SomeString: 'yo',
 		SomeInteger: 123,
 		SomeDecimal: 1.23,
-		SomeDate: new Date()
+		SomeDate: new Date().toISOString(),
+		PingField: 'PING'
 	});
+
 });
 
 bus.init({
   host: 'rabbitmq-test',
   queueNames: ['ping-test','ping-test2'],
-  transport: 'amqp'
+  transport: 'amqp',
+	publishMessageTypes: {
+		"PingMassTransit:Ping": [
+			'PingMassTransit:Ping',
+			'PingMassTransit:IPing'
+		]
+	}
 });
